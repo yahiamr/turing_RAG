3c37349be8adebf3f0c833d7017f4f08ea354a33
yahiamr <146677961+yahiamr@users.noreply.github.com>
2024-03-24 23:14:41 +0200

    Update chat.py

--- streamlit/chat.py
+++ streamlit/chat.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
 
-message = st.chat_message("assistant")
-message.write("Hello human")
-message.bar_chart(np.random.randn(30, 3))
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
