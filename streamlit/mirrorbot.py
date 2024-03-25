944b6864cae2ef0019482b5c8f26a8631d003670
yahiamr <146677961+yahiamr@users.noreply.github.com>
2024-03-25 23:30:58 +0200

    Update mirrorbot.py

--- streamlit/mirrorbot.py
+++ streamlit/mirrorbot.py
@@ -1,1 +1,12 @@
-#bot to mirror input
+import streamlit as st
+
+st.title("Echo Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
