542558ba6a763b55e32ae5b8a79ba563f088f7e7
yahiamr <146677961+yahiamr@users.noreply.github.com>
2024-03-26 23:05:15 +0200

    Update mirrorbot.py

--- streamlit/mirrorbot.py
+++ streamlit/mirrorbot.py
@@ -10,3 +10,11 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+    
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
