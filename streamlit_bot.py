2e5a797e7ef227931ec088691114d2ce960a36a8
yahiamr <146677961+yahiamr@users.noreply.github.com>
2024-04-14 23:14:45 +0200

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 # Load tokenizer and model only once, using caching
-@st.cache(allow_output_mutation=True)
+@st.cache(allow_output_mutation=False)
 def load_model():
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     model = AutoModelForCausalLM.from_pretrained("gpt2")
